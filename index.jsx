ece59db94d1b83a274ff5ad755a1b1ac10239906
Lucas <lucas@agtlucas.com>
2015-04-26 08:17:11 -0300

    Rename index.js to .jsx

--- index.jsx
+++ index.jsx
@@ -1,7 +1,6 @@
-/** @jsx React.DOM */
 setTimeout(function() {
   var React = require('react');
-  var Timer = require('./Timer');
+  var Timer = require('/Timer');
 
   React.renderComponent(<Timer />, document.getElementById('react-root'));
 }, 3000);
